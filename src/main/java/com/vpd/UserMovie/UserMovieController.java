0d76f970096aef7209f346cdd46f4beae6d4524d
GabrielSiek <gabriels2606@gmail.com>
2025-04-16 00:52:11 -0300

    adding basic usermovie controller specs

---
+++ src/main/java/com/vpd/UserMovie/UserMovieController.java
@@ -0,0 +1,21 @@
+package com.vpd.UserMovie;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("movie")
+public class UserMovieController {
+
+    //favoritar
+
+    //desfavoritar
+
+    //assistir
+
+    //dessassistir
+
+    //dar nota
+
+    //remover nota
+}
