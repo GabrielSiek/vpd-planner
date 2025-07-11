d106b22d010740929ed1fc76b11798d7dca4990d
Gabriel Siek <gabriels2606@gmail.com>
2025-07-11 01:46:44 -0300

    allow all requests origins

--- src/main/java/com/vpd/Configuration/CorsConfiguration.java
+++ src/main/java/com/vpd/Configuration/CorsConfiguration.java
@@ -7,12 +7,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:5173")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+
+    //API publica
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**") // aplica para todos os endpoints
+                .allowedOrigins("*") // aceita requisições de qualquer origem
+                .allowedMethods("*") // permite todos os métodos HTTP
+                .allowedHeaders("*") // permite todos os headers
+                .allowCredentials(false); // precisa ser false se allowedOrigins == "*"
     }
 }
